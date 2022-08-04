4673fc47bfb6a1487845bc043a436b951c7bc0f2
Ricardo Maya <rickmaya@gmail.com>
2022-08-04 13:26:50 -0500

    export logEvents module and create erroHandle module

--- src/middleware/logEvents.js
+++ src/middleware/logEvents.js
@@ -10,12 +10,14 @@ const logEvents = async (message, logName) => {
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
   try {
-    if (!fs.existsSync(path.join(__dirname, 'logs'))) {
-      await fsPromise.mkdir(path.join(__dirname, 'logs'));
+    if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+      await fsPromise.mkdir(path.join(__dirname, '..', 'logs'));
     }
 
-    await fsPromise.appendFile(path.join(__dirname, 'logs', logName), logItem);
+    await fsPromise.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
   } catch (err) {
     console.log(err);
   }
 };
+
+module.exports = logEvents;
