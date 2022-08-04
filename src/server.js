6ce7392ef43aecc0f1f15a33b10359e66254bfa6
Ricardo Maya <rickmaya@gmail.com>
2022-08-04 14:31:00 -0500

    import cors and create an options object for it

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const connectDB = require('./config/dbSetup');
@@ -11,12 +12,34 @@ const userRoutes = require('./routes/api/users');
 // custom middleware logger
 app.use(logger);
 
+// Cross Origin Resource Sharing
+// may want to remove after development---
+const whitelist = [
+  'https://www.yoursite.com',
+  'http://127.0.0.1:5000',
+  'http://localhost:5000'
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+}
+// may want to remove after development---^
+app.use(cors(corsOptions));
+
 // Built-in middleware to handle urlencoded data
 // in other words, form data:
 // 'content-type: application/x-www-form-urlencoded'
 app.use(express.urlencoded({extended: false}));
+
 // Built-in middleware for json
 app.use(express.json());
+
 // Serve static files
 app.use(express.static(path.join(__dirname, '/public')));
 //======================
