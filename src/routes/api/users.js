128e4e039d9b845f48dd4e77a0b04ccec9947e7c
Ricardo Maya <rickmaya@gmail.com>
2022-08-09 18:27:18 -0500

    delete old auth file/adjust retrieve controller to find user by email

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../../controllers/userControllers');
+const userController = require('../../controllers/userControllers');
 const auth = require('../../middleware/auth');
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post('/register', User.register);
+router.post('/register', userController.register);
 
 // @route POST api/users/login
 // @desc Login user
 // @access Public
-router.post('/login', User.login);
+router.post('/login', userController.login);
 
 // @route GET api/user/info
 // @desc GET user info
 // @access Public
-router.get('/user/info', auth, User.retrieve);
+router.get('/user/info', userController.retrieve);
 
 module.exports = router;
