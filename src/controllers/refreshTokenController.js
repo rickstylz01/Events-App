78ed278c4e8d732e8d088b3539b3744f2d8457a1
Ricardo Maya <rickmaya@gmail.com>
2022-08-11 16:48:07 -0500

    modify: use await when finding a user in refreshTokenController

--- src/controllers/refreshTokenController.js
+++ src/controllers/refreshTokenController.js
@@ -4,24 +4,22 @@ const jwt = require('jsonwebtoken');
 const handleRefreshToken = async (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401);
-  console.log(cookies.jwt);
   const refreshToken = cookies.jwt;
 
   // find user by the refresh token
-  const foundUser = User.findOne({ refreshToken }).exec();
+  const foundUser = await User.findOne({ refreshToken }).exec();
   if (!foundUser) return res.sendStatus(403);
-
   // evaluate jwt
   jwt.verify(
     refreshToken,
     process.env.REFRESH_TOKEN_SECRET,
     (err, decoded) => {
-      if (err || foundUser.name !== decoded.name) return res.sendStatus(403);
+      if (err || foundUser.email !== decoded.email) return res.sendStatus(403);
       const roles = Object.values(foundUser.roles);
       const accessToken = jwt.sign(
         {
           "UserInfo": {
-            "Username": decoded.name,
+            "email": decoded.email,
             "roles": roles
           }
         },
