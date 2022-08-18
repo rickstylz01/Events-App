457287c455935e9481369a90180e56f4f715ed3c
Ricardo Maya <rickmaya@gmail.com>
2022-08-18 13:20:16 -0500

    modify: import browser router into root index file and wrap components in app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Navbar from "./components/layout/Navbar/Navbar";
 import Landing from './components/layout/Landing/Landing';
+import {Route, Routes} from "react-router-dom";
+import Register from "./components/auth/Register/Register";
+import Login from "./components/auth/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-    </div>
+    <Routes>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" element={<Landing />}/>
+        <Landing />
+      </div>
+    </Routes>
   );
 }
 
