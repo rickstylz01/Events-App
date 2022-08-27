2b802557b943fc9040a53a068dbd3ffd41012e4f
Ricardo Maya <rickmaya@gmail.com>
2022-08-27 15:46:56 -0500

    feature: create profile page component and include image and card. remove bootstrap cdn

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import Navbar from "./components/layout/Navbar/Navbar";
 import Landing from './components/layout/Landing/Landing';
 import Register from "./components/auth/Register/Register";
 import { Route, Routes } from "react-router-dom";
-import RequireAuth from "./components/RequireAuth";
+// import RequireAuth from "./components/RequireAuth";
 import Login from "./components/auth/Login/Login";
 import Layout from "./components/layout/Layout";
+import ProfileCard from "./components/auth/Profile";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Landing />}/>
           <Route path="register" element={<Register />}/>
           <Route path="login" element={ <Login />}/>
+          <Route path="profile" element={ <ProfileCard /> } />
         </Route>
       </Routes>
     </main>
