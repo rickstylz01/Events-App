d6d1f72a275107cb05202ea36b2e62a6a64e9d82
Ricardo Maya <rickmaya@gmail.com>
2022-08-27 17:46:47 -0500

    modify: return found user json object in getUser controller

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -26,7 +26,7 @@ const deleteUser = async (req, res) => {
   if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
   const user = await User.findOne({ _id: req.body.id }).exec()
   if (!user) {
-    return res.status(204).json({ 'message': `User ID ${req.body.id} not found`});
+    return res.status(204).json({ 'message': `No user matches ID ${req.body.id}.`});
   }
   const result = await user.deleteOne({ _id: req.body.id });
   res.json(result);
@@ -36,12 +36,14 @@ const getUser = async (req, res) => {
   if (!req.params?.id) return res.status(400).json({ 'message': 'User ID required' });
   const user = await User.findOne({ _id: req.params.id }).exec();
   if (!user) {
-    return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
+    return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
   }
+  res.json(user);
 }
 
 module.exports = {
   getAllUsers,
+  updateUser,
   getUser,
   deleteUser
 }
