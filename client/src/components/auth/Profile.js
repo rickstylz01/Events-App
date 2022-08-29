9ae686518a8e8aff2af6d2709d7551faac61824c
Ricardo Maya <rickmaya@gmail.com>
2022-08-29 16:40:25 -0500

    modify: create logout button and place in profile page to let the user logout

--- client/src/components/auth/Profile.js
+++ client/src/components/auth/Profile.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import bboyImg from '../../assets/bboy.JPG';
+import AuthContext from "../../context/AuthProvider";
+import {useNavigate} from "react-router-dom";
 
 const ProfileCard = () => {
+  const { setAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    // if used in more components, this should be in context
+    // axios to /logout endpoint
+    setAuth({});
+    navigate('/')
+  }
+
   return (
     <div className="card">
       <div className="card-image waves-effect waves-block waves-light">
@@ -17,8 +29,11 @@ const ProfileCard = () => {
         <span className="card-title grey-text text-darken-4">Card Title<i
           className="material-icons right">close</i></span>
         <p>Here is some more information about this product that is only revealed once clicked on.</p>
-        <button className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-          Logout
+        <button
+          className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+          onClick={logout}
+        >
+          Sign Out
         </button>
       </div>
     </div>
